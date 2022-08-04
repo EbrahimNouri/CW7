f63be7798ed240169fd3c04356f9a9f1ddf561ba
ebrahim <viroos@live.com>
2022-08-04 16:47:33 +0430

    v 1

--- src/main/java/entity/Library.java
+++ src/main/java/entity/Library.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    private List<Usertype> userList = new ArrayList<>() {
-    };
+    private List<UserAccount> userList;
+
 
 }
